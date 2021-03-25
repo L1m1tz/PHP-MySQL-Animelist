59f127c35c4c8230d5339e5d66d074dde8fa3f17
L1m1tz <andreew.mk@gmail.com>
2021-03-25 13:29:26 +0200

    fixed create & index to depay data

--- create.php
+++ create.php
@@ -2,68 +2,180 @@
 // Include config file
 require_once "config.php";
 
+$stmt = $pdo->query("SELECT * FROM type");
+$types = $stmt->fetchAll();
+
+/**
+ * Undocumented function
+ *
+ * @param string $id
+ * @param array $validAliasModel
+ * @return boolean
+ */
+function checkIds(string $id, array $validAliasModel): bool
+{
+    $validItemArray = array_filter($validAliasModel, function ($model) use ($id) {
+        return $model['id'] === $id;
+    });
+
+    return count($validItemArray) > 0;
+}
+
 // Define variables and initialize with empty values
-$name = $address = $salary = "";
-$name_err = $address_err = $salary_err = "";
+$anime_name = $dub_name = $season_no = $description = $season_statuses = $release_date = $rating = $licensors = $link = $type = "";
+$anime_name_err = $dub_name_err = $season_no_err = $description_err = $release_date_err = $season_statuses_err = $rating_err = $licensors_err = $link_err = $new_release_date_err = $type_err = "";
 
 // Processing form data when form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Validate name
-    $input_name = trim($_POST["name"]);
-    if (empty($input_name)) {
-        $name_err = "Please enter a name.";
-    } elseif (!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/^[a-zA-Z\s]+$/")))) {
-        $name_err = "Please enter a valid name.";
+
+    // Validate anime name
+    $input_anime_name = trim($_POST["anime_name"]);
+    if (empty($input_anime_name)) {
+        $anime_name_err = "Please enter anime name.";
+    } elseif (!filter_var($input_anime_name, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/^[a-zA-Z\s]+$/")))) {
+        $anime_name_err = "Please enter a valid anime name.";
     } else {
-        $name = $input_name;
+        $anime_name = $input_anime_name;
     }
 
-    // Validate address
-    $input_address = trim($_POST["address"]);
-    if (empty($input_address)) {
-        $address_err = "Please enter an address.";
+    // Validate anime dub title
+    $input_dub_name = trim($_POST["dub_name"]);
+    if (empty($input_dub_name)) {
+        $dub_name_err = "Please enter an address.";
     } else {
-        $address = $input_address;
+        $dub_name = $input_dub_name;
     }
 
-    // Validate salary
-    $input_salary = trim($_POST["salary"]);
-    if (empty($input_salary)) {
+    // Validate season number
+    $input_season_no = trim($_POST["season_no"]);
+    if (empty($input_season_no)) {
         $salary_err = "Please enter the salary amount.";
-    } elseif (!ctype_digit($input_salary)) {
-        $salary_err = "Please enter a positive integer value.";
+    } elseif (!ctype_digit($input_season_no)) {
+        $season_no_err = "Please enter a positive integer value.";
     } else {
-        $salary = $input_salary;
+        $season_no = $input_season_no;
     }
+
+    // Validate description
+    $input_description = trim($_POST["description"]);
+    if (empty($input_description)) {
+        $description_err = "Please enter a Description.";
+    } else {
+        $description = $input_description;
+    }
+
+    // Validate type
+    $input_type = isset($_POST["type"]) ? trim($_POST['type']) : null;
+    if (empty($input_type) && !checkIds($input_type, $types)) {
+        $type_err = "Please select a valid type.";
+    } else {
+        $type = $input_type;
+    }
+
+    // Validate season status
+    $input_season_statuses = isset($_POST["season_status"]) ? trim($_POST["season_status"]) : null;
+    if (empty($input_season_statuses)) {
+        $season_statuses_err = "Please enter Season Status.";
+    } else {
+        $season_statuses = $input_season_statuses;
+    }
+
+    // Validate release_date
+    $input_release_date = trim($_POST["release_date"]);
+    if (empty($input_release_date) || !preg_match("/\d{4}-([0]\d|[1][0-2])-([0-2]\d|[3][0-1])/", $input_release_date) || strlen($input_release_date) > 10) {
+        $release_date_err = "Please enter a release date.";
+    } else {
+        //$new_release_date = date("YYYY-mm-dd", strtotime($input_release_date));
+        $release_date = $input_release_date;
+    }
+
+    // Validate rating
+    $input_rating = trim($_POST["rating"]);
+    if (empty($input_rating)) {
+        $rating_err = "Please enter rating.";
+    } else {
+        $rating = $input_rating;
+    }
+
+    // Validate licensors
+    $input_licensors = trim($_POST["licensors"]);
+    if (empty($input_licensors)) {
+        $licensors_err = "Please enter a licensor.";
+    } else {
+        $licensors = $input_licensors;
+    }
+
+    // validate link
+    $input_link = trim($_POST["link"]);
+    if (empty($_POST["link"])) {
+        $link_err = "";
+    } else {
+        $link = $input_link;
+    }
+    // if (filter_var($input_link, FILTER_VALIDATE_URL)) {
+    //     echo ("$ is a valid URL");
+    // } else {
+    //     echo ("$input_link is not a valid URL");
+    // }
+
 
     // Check input errors before inserting in database
-    if (empty($name_err) && empty($address_err) && empty($salary_err)) {
+    if (empty($anime_name_err) && empty($dub_name_err) && empty($season_no_err) && empty($description_err) && empty($release_date_err) && empty($season_statuses_err) &&  empty($rating_err) && empty($licensors_err) && empty($link_err)) {
+
+        // Set parameters
+        $param_anime_name = $anime_name;
+        $param_dub_name = $dub_name;
+        $param_season_no = $season_no;
+        $param_description = $description;
+        $param_release_date = $release_date;
+        $param_season_statuses = $season_statuses;
+        $param_rating = $rating;
+        $param_licensors = $licensors;
+        $param_link = $link;
+        $param_type = $type;
+
         // Prepare an insert statement
-        $sql = "INSERT INTO employees (name, address, salary) VALUES (:name, :address, :salary)";
+        $sql = "INSERT INTO anime_show (anime_name, type_id) VALUES (:anime_name, :type_id)";
 
         if ($stmt = $pdo->prepare($sql)) {
             // Bind variables to the prepared statement as parameters
-            $stmt->bindParam(":name", $param_name);
-            $stmt->bindParam(":address", $param_address);
-            $stmt->bindParam(":salary", $param_salary);
-
-            // Set parameters
-            $param_name = $name;
-            $param_address = $address;
-            $param_salary = $salary;
-
-            // Attempt to execute the prepared statement
-            if ($stmt->execute()) {
-                // Records created successfully. Redirect to landing page
-                header("location: index.php");
-                exit();
-            } else {
-                echo "Oops! Something went wrong. Please try again later.";
-            }
+            $stmt->bindParam(":anime_name", $param_anime_name);
+            $stmt->bindParam(":type_id", $param_type);
         }
+
+        $showCreated = $stmt->execute();
+        $showId = $pdo->lastInsertId();
+
+        $sql2 = "INSERT INTO  seasons (show_id, dub_name, season_no, description, season_status_id, release_date, rating, licensors, link ) 
+        VALUES (:show_id, :dub_name, :season_no, :description, :season_status_id, :release_date, :rating, :licensors, :link )";
+
+        if ($stmt2 = $pdo->prepare($sql2)) {
+            $stmt2->bindParam(":show_id", $showId);
+            $stmt2->bindParam(":dub_name", $param_dub_name);
+            $stmt2->bindParam(":season_no", $param_season_no);
+            $stmt2->bindParam(":description", $param_description);
+            $stmt2->bindParam(":season_status_id", $param_season_statuses);
+            $stmt2->bindParam(":release_date", $param_release_date);
+            $stmt2->bindParam(":rating", $param_rating);
+            $stmt2->bindParam(":licensors", $param_licensors);
+            $stmt2->bindParam(":link", $param_link);
+        }
+
+        $seasonCreated = $stmt2->execute();
+
+        // Attempt to execute the prepared statement
+        if ($showCreated && $seasonCreated) {
+            // Records created successfully. Redirect to landing page
+            header("location: index.php");
+            exit();
+        } else {
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+
 
         // Close statement
         unset($stmt);
+        unset($stmt2);
     }
 
     // Close connection
@@ -78,12 +190,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <title>Create Record</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        .wrapper {
-            width: 600px;
-            margin: 0 auto;
-        }
-    </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -91,24 +199,100 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12">
-                    <h2 class="mt-5">Create Record</h2>
-                    <p>Please fill this form and submit to add employee record to the database.</p>
+                    <h2 class="mt-5">Create Anime Record</h2>
+                    <p>Please fill this form and submit to add anime record to the database.</p>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                         <div class="form-group">
-                            <label>Name</label>
-                            <input type="text" name="name" class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $name; ?>">
-                            <span class="invalid-feedback"><?php echo $name_err; ?></span>
+
+                            <!--Anime name-->
+                            <label>Anime Title</label>
+                            <input type="text" name="anime_name" class="form-control <?php echo (!empty($anime_name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $anime_name; ?>">
+                            <span class="invalid-feedback"><?php echo $anime_name_err; ?></span>
                         </div>
+
+                        <!-- anime dub name-->
                         <div class="form-group">
-                            <label>Address</label>
+                            <label>Anime Dub Title</label>
+                            <textarea name="dub_name" class="form-control <?php echo (!empty($dub_name_err)) ? 'is-invalid' : ''; ?>"><?php echo $dub_name; ?></textarea>
+                            <span class="invalid-feedback"><?php echo $dub_name_err; ?></span>
+                        </div>
+
+                        <!-- Season number-->
+                        <div class="form-group">
+                            <label>Season Number</label>
+                            <textarea name="season_no" class="form-control <?php echo (!empty($season_no_err)) ? 'is-invalid' : ''; ?>"><?php echo $season_no; ?></textarea>
+                            <span class="invalid-feedback"><?php echo $season_no_err; ?></span>
+                        </div>
+
+                        <!--Details-->
+                        <div class="form-group">
+                            <label>Plot</label>
+                            <textarea type="text" name="description" class="form-control <?php echo (!empty($description_err)) ? 'is-invalid' : ''; ?>"> <?php echo $description; ?></textarea>
+                            <span class="invalid-feedback"><?php echo $description_err; ?></span>
+                        </div>
+
+                        <!--Anime Type-->
+                        <label>Anime Type</label>
+                        <select name="type" class="form-select" aria-label="Default select example">
+                            <option selected value="">Open this select menu</option>
+
+                            <?php foreach ($types as $type) : ?>
+                                <option value="<?= $type['id']; ?>"><?= $type['name']; ?></option>
+                            <?php endforeach; ?>
+
+
+                        </select>
+
+                        <br>
+
+                        <!--status-->
+                        <div class="form-group">
+                            <label>Status</label>
+                            <!--<textarea type="checkbox" name="season_status" class="form-control <?php echo (!empty($season_status_err)) ? 'is-invalid' : ''; ?>"><?php echo $season_status; ?></textarea>-->
+                            <input type="radio" name="season_status" <?php if (isset($season_status) && $season_status == "1") echo "checked"; ?> value="1"> Ongoing <br>
+                            <input type="radio" name="season_status" <?php if (isset($season_status) && $season_status == "2") echo "checked"; ?> value="2"> Complete
+                            <span class="invalid-feedback"><?php echo $season_status_err; ?></span>
+                        </div>
+
+                        <!--release date-->
+                        <div class="form-group">
+                            <label>Date</label>
+                            <input type="text" name="release_date" class="form-control <?php echo (!empty($release_date_err)) ? 'is-invalid' : ''; ?>"><?php echo $release_date; ?></textarea>
+                            <span class="invalid-feedback"><?php echo $release_date_err; ?></span>
+                        </div>
+
+                        <!--
+                            <div class="form-group">
+                            <label>Genre</label>
                             <textarea name="address" class="form-control <?php echo (!empty($address_err)) ? 'is-invalid' : ''; ?>"><?php echo $address; ?></textarea>
                             <span class="invalid-feedback"><?php echo $address_err; ?></span>
                         </div>
                         <div class="form-group">
-                            <label>Salary</label>
-                            <input type="text" name="salary" class="form-control <?php echo (!empty($salary_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $salary; ?>">
-                            <span class="invalid-feedback"><?php echo $salary_err; ?></span>
+                            <label>Type</label>
+                            <textarea name="address" class="form-control <?php echo (!empty($address_err)) ? 'is-invalid' : ''; ?>"><?php echo $address; ?></textarea>
+                            <span class="invalid-feedback"><?php echo $address_err; ?></span>
+                        </div> 
+                        <div class="form-group">
+                            <label>Episodes</label>
+                            <textarea name="address" class="form-control <?php echo (!empty($address_err)) ? 'is-invalid' : ''; ?>"><?php echo $address; ?></textarea>
+                            <span class="invalid-feedback"><?php echo $address_err; ?></span>
+                        </div>-->
+                        <div class="form-group">
+                            <label>Rating</label>
+                            <input type="number" name="rating" class="form-control <?php echo (!empty($rating_err)) ? 'is-invalid' : ''; ?>"><?php echo $rating; ?></textarea>
+                            <span class="invalid-feedback"><?php echo $rating_err; ?></span>
                         </div>
+                        <div class="form-group">
+                            <label>Licensors</label>
+                            <textarea name="licensors" class="form-control <?php echo (!empty($licensorss_err)) ? 'is-invalid' : ''; ?>"><?php echo $licensors; ?></textarea>
+                            <span class="invalid-feedback"><?php echo $licensors_err; ?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Website Link</label>
+                            <textarea name="link" class=" form-control <?php echo (!empty($link_err)) ? 'is-invalid' : ''; ?>"><?php echo $link; ?></textarea>
+                            <span class="invalid-feedback"><?php echo $link_err; ?></span>
+                        </div>
+
                         <input type="submit" class="btn btn-primary" value="Submit">
                         <a href="index.php" class="btn btn-secondary ml-2">Cancel</a>
                     </form>
--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<?php
+// Include config file
+require_once "config.php";
+
+// Attempt select query execution
+$sql = "SELECT *,
+            seasons.id AS season_id
+            FROM seasons
+            JOIN anime_show on anime_show.id = seasons.show_id";
+
+$seasonStatement = $pdo->query($sql);
+
+$seasons = $seasonStatement->fetchAll();
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,19 +22,11 @@
   <title>Dashboard</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
   <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <style>
-    .wrapper {
-      width: 600px;
-      margin: 0 auto;
-    }
-
-    table tr td:last-child {
-      width: 120px;
-    }
-  </style>
   <script>
     $(document).ready(function() {
       $('[data-toggle="tooltip"]').tooltip();
@@ -27,56 +35,57 @@
 </head>
 
 <body>
-  <div class="wrapper">
+  <div class="container">
     <div class="container-fluid">
       <div class="row">
         <div class="col-md-12">
           <div class="mt-5 mb-3 clearfix">
-            <h2 class="pull-left">Employees Details</h2>
-            <a href="create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Add New Employee</a>
+            <h2 class="pull-left">Anime Details</h2>
+            <a href="create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Add New Anime</a>
           </div>
           <?php
-          // Include config file
-          require_once "config.php";
+          if (!!$seasons) {
+            if (count($seasons) > 0) { ?>
+              <table class="table table-bordered table-striped table-hover">
 
-          // Attempt select query execution
-          $sql = "SELECT * FROM employees";
-          if ($result = $pdo->query($sql)) {
-            if ($result->rowCount() > 0) {
-              echo '<table class="table table-bordered table-striped">';
-              echo "<thead>";
-              echo "<tr>";
-              echo "<th>#</th>";
-              echo "<th>Name</th>";
-              echo "<th>Address</th>";
-              echo "<th>Salary</th>";
-              echo "<th>Action</th>";
-              echo "</tr>";
-              echo "</thead>";
-              echo "<tbody>";
-              while ($row = $result->fetch()) {
-                echo "<tr>";
-                echo "<td>" . $row['id'] . "</td>";
-                echo "<td>" . $row['name'] . "</td>";
-                echo "<td>" . $row['address'] . "</td>";
-                echo "<td>" . $row['salary'] . "</td>";
-                echo "<td>";
-                echo '<a href="read.php?id=' . $row['id'] . '" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
-                echo '<a href="update.php?id=' . $row['id'] . '" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
-                echo '<a href="delete.php?id=' . $row['id'] . '" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
-                echo "</td>";
-                echo "</tr>";
-              }
-              echo "</tbody>";
-              echo "</table>";
-              // Free result set
-              unset($result);
-            } else {
-              echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
-            }
-          } else {
-            echo "Oops! Something went wrong. Please try again later.";
-          }
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Anime Name</th>
+                    <th>Plot</th>
+                    <th>Season</th>
+                    <th>Release Date</th>
+                    <th>Type</th>
+                    <th>rating</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php
+                  foreach ($seasons as $season) { ?>
+                    <tr>
+                      <td><?php echo $season['show_id'] ?></td>
+                      <td><?php echo $season['anime_name'] ?></td>
+                      <td><?php echo $season['description'] ?></td>
+                      <td><?php echo $season['season_no'] ?></td>
+                      <td><?php echo $season['release_date'] ?></td>
+                      <td><?php echo $season['type_id'] ?></td>
+                      <td><?php echo $season['rating'] ?></td>
+                      <td>
+                        <a href="read.php?id=<?php echo $season['show_id'] ?>" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>
+                        <a href="update.php?id=<?php echo $season['show_id'] ?>" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>
+                        <a href="delete.php?id=<?php echo $season['show_id'] ?>" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>
+                      </td>
+                    </tr>
+                  <?php } ?>
+                </tbody>
+              </table>
+            <?php } else { ?>
+              <div class="alert alert-danger"><em>No records were found.</em></div>
+            <?php }
+          } else { ?>
+            <div class="alert alert-danger"><em>Oops! Something went wrong. Please try again later.</em></div>
+          <?php }
 
           // Close connection
           unset($pdo);
