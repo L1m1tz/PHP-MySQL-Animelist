92ee3c613eccedda24ccd47655c8c0bb2eb36d74
L1m1tz <andreew.mk@gmail.com>
2021-04-08 14:11:53 +0200

    var_dump function as dd

--- config.php
+++ config.php
@@ -4,12 +4,18 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'password');
 define('DB_NAME', 'anime');
- 
+
 /* Attempt to connect to MySQL database */
-try{
+try {
     $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
     // Set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e){
+} catch (PDOException $e) {
     die("ERROR: Could not connect. " . $e->getMessage());
+}
+
+function dd($var)
+{
+    var_dump($var);
+    exit;
 }
