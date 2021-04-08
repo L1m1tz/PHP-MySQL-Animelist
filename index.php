ef2c8430607e882579a6b24116c750b4b9aa2707
L1m1tz <andreew.mk@gmail.com>
2021-04-08 14:12:50 +0200

    add switch to index for proper link directing

--- index.php
+++ index.php
@@ -1,3 +1,62 @@
-<?php header("Location: http://animelist.test/shows/show_list.php");
-die();
+<?php
+require_once "config.php";
+
+$content = 'shows/show_list.php';
+$title = 'Show List';
+
+if (isset($_GET['page'])) {
+    $page = strip_tags($_GET['page']);
+
+    switch ($page) {
+        case '':
+        case 'show-list':
+            break;
+
+        case 'show-create':
+            $title = 'Create A Show';
+            $content = 'shows/show_create.php';
+            break;
+
+        case 'show-delete':
+            $title = 'Create A Show';
+            $content = 'shows/show_delete.php';
+            break;
+
+        case 'season-list':
+            $title = 'Season List';
+            $content = 'seasons/season_list.php';
+            break;
+        case 'season-view':
+            $title = 'View A Season';
+            $content = 'seasons/season_view.php';
+            break;
+
+        case 'season-create':
+            $title = 'Create A Season';
+            $content = 'seasons/season_create.php';
+            break;
+
+        case 'season-delete':
+            $title = 'Delete A Season';
+            $content = 'seasons/season_delete.php';
+            break;
+
+        case 'reset':
+            $title = 'Reset Database';
+            $content = 'reset.php';
+            break;
+        default:
+            $content = 'view/404.php';
+            $title = 'Page Doesn\'t Exist';
+            break;
+    }
+}
+
+
+include_once('view/header.php');
 ?>
+<div class="wrapper">
+    <?php include_once($content); ?>
+</div>
+<?php
+include_once('view/footer.php');
