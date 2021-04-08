9e403ad73bf6e2b8220aef28cb146585cdb5904f
L1m1tz <andreew.mk@gmail.com>
2021-04-08 14:16:52 +0200

    title and navbar added

---
+++ view/title.php
@@ -0,0 +1,7 @@
+<div class="container-fluid p-3">
+    <div class="row justify-content-center">
+        <div class="col-6 text-center">
+            <h1><?php echo $title ?></h1>
+        </div>
+    </div>
+</div>
